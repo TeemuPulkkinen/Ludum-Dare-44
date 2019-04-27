d5e798380e868a7422f7299eda2c08d8b6a44460
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-04-28 01:17:48 +0300

    GameManager planning and MainMenu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
-    
 
+    [SerializeField]
+    private int level;
+    [SerializeField]
+    private bool lastLevel;
+    private int nextLevel;
+
+    // Awake is called before start
     private void Awake()
     {
         DontDestroyOnLoad(this);
     }
 
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextLevel = level + 1;
+    }
 
-    
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadNextLevel()
+    {
+        if (!lastLevel)
+        {
+            string sceneName = "Level-" + nextLevel;
+
+            LoadLevel(sceneName);
+        }
+        else
+        {
+            // Go to main menu.
+            LoadLevel("Main-Menu");
+        }
+
+
+    }
+
+    public void ReloadCurrentLevel()
+    {
+        LoadLevel("Level-" + level);
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
+
 }
