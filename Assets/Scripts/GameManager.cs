ff9c76064790fa9815f5d5603c125030aceff051
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-04-27 16:38:56 +0300

    Healthbar added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Pelaajan elämäpisteiden tekstikenttä
+    [SerializeField]
+    private Text healthText;
+    private float currentHealth;
+    private float newHealth;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    
+
+    public void UpdateHealth()
+    {
+        healthText.text = "Testi";
+    }
+}
---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public Slider healthBarSlider;
+    public float currentHealth;
+    public float maxHealth;
+    public Text healthText;
+    public Player player;
+    bool isDead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = player.playerHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBarSlider.maxValue = maxHealth;
+        healthBarSlider.value = currentHealth;
+        healthText.text = currentHealth.ToString() + " / " + maxHealth.ToString();
+
+        if (currentHealth <= 0)
+        {
+            if (isDead)
+            {
+                return;
+            }
+            Dead();
+        }
+        /*if (Input.GetKeyDown(KeyCode.T))
+        {
+            currentHealth -= 10;
+        }*/
+
+        currentHealth -= 1;
+    }
+
+    void Dead()
+    {
+        isDead = true;
+    }
+}
