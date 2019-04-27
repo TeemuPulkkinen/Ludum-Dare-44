c0d0b2b92520703b1d9f2994af5873e1bb56db39
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-04-27 11:03:49 +0300

    Player movement added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody playerBody;
+    private bool jump;
+
+    private Vector3 inputVector;
+    private float inputX;
+    private float inputZ;
+
+    private Vector3 targetVector;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * 10f * Time.deltaTime, playerBody.velocity.y, Input.GetAxis("Vertical") * 10f * Time.deltaTime);
+        //transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+
+        targetVector = transform.position + inputVector;
+
+        playerBody.MovePosition(targetVector);
         
+    }
+
+    private void FixedUpdate() // Called once in 50 frames
+    {
+        playerBody.velocity = inputVector;
     }
 }
