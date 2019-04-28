371baa0375822c69e1d71c7bfb870dc8345ab438
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-04-28 06:18:44 +0300

    Added a Gateway
    A Gateway script to use as a "goal" of the level and a script that transitions the player to next level.

---
+++ Assets/Scripts/Gateway.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gateway : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameManager.LoadLevel("Test_Scene_1");
+        }
+    }
+}
